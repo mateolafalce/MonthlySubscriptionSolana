99bc921b296a4e57c0ccb752d561ab00c7df0c67
Mateo Lafalce <mateolafalce3@gmail.com>
2023-04-18 00:08:12 +1100

    suscribe fn

--- monthly_subscription_solana/src/instructions/suscribe.rs
+++ monthly_subscription_solana/src/instructions/suscribe.rs
@@ -1,36 +1,48 @@
+// Import necessary libraries
 use anchor_lang::{
     prelude::*,
     solana_program::pubkey::Pubkey,
     solana_program::system_instruction,
 };
+// Import custom modules
 use crate::state::accounts::*;
 use crate::errors::ErrorCode;
 
+// Define function to subscribe
 pub fn suscribe(
     ctx: Context<Suscribe>,
     name: String,
     lastname: String
 ) -> Result<()> {
+    // Check if name or lastname are too long
     if name.chars().count() > 20 {
         return Err(ErrorCode::TooLong.into())
     }
     if lastname.chars().count() > 20 {
         return Err(ErrorCode::TooLong.into())
     }
+    // Get mutable reference to user_data account
     let user_data: &mut Account<SubscriberData> = &mut ctx.accounts.user_data;
+    // Generate PDA and bump using the keys of the enterprise and user accounts
     let (_pda, bump) = Pubkey::find_program_address(&[ctx.accounts.enterprise_data.key().as_ref(), ctx.accounts.user.key().as_ref()], ctx.program_id);
+    // Transfer funds from 'from' account to the enterprise authority account
     anchor_lang::solana_program::program::invoke(
         &system_instruction::transfer(&ctx.accounts.from.key(), &ctx.accounts.enterprise_data.authority, ctx.accounts.enterprise_data.amount_per_month),
         &[ctx.accounts.from.to_account_info(), ctx.accounts.stake.to_account_info().clone()],
     ).expect("Error");
+    // Get mutable reference to enterprise_data account
     let enterprise_data: &mut Account<EnterpriseData> = &mut ctx.accounts.enterprise_data;
+    // Increment total_users count
     enterprise_data.total_users += 1;
+    // Set user_data bump, month_timestamp, and credits
     user_data.bump = bump;
     user_data.month_timestamp = Clock::get().unwrap().unix_timestamp + 2592000;
     user_data.credits = 8;
+    // Return Ok if everything is successful
     Ok(())
 }
 
+// Define accounts required for subscription
 #[derive(Accounts)]
 pub struct Suscribe<'info> {
     #[account(mut, seeds = [b"Enterprise", enterprise_data.authority.key().as_ref()], bump = enterprise_data.bump_original)]
