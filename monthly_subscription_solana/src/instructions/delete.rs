93bc3a8e1a00a0e194db311cab955898e1746cc3
Mateo Lafalce <mateolafalce3@gmail.com>
2023-04-19 00:53:43 +1100

    readme delete

--- monthly_subscription_solana/src/instructions/delete.rs
+++ monthly_subscription_solana/src/instructions/delete.rs
@@ -19,14 +19,11 @@ pub struct Delete<'info> {
     // The enterprise_data account must be mutable and is derived from a seed
     #[account(mut, seeds = [b"Enterprise", enterprise_data.authority.key().as_ref()], bump = enterprise_data.bump_original)]
     pub enterprise_data: Account<'info, EnterpriseData>,
-
     // The user_data account must be mutable, is derived from two seeds, and will be closed upon deletion
     #[account(mut, seeds = [enterprise_data.key().as_ref(), user.key().as_ref()], bump = user_data.bump, close = enterprise_data)]
     pub user_data: Account<'info, SubscriberData>,
-
     // The user account must be a signer (i.e. it must be authorized to perform the delete operation)
     pub user: Signer<'info>,
-
     // The system_program account is a standard Solana program account used for system-level operations
     pub system_program: Program<'info, System>,
 }
