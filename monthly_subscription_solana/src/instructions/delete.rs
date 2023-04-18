1d21adf05cb28faa10a3d8b1fad16ccfd4150db1
Mateo Lafalce <mateolafalce3@gmail.com>
2023-04-19 00:52:42 +1100

    delete fn

--- monthly_subscription_solana/src/instructions/delete.rs
+++ monthly_subscription_solana/src/instructions/delete.rs
@@ -1,20 +1,32 @@
 use anchor_lang::{
     prelude::*,
     solana_program::pubkey::Pubkey,
-}; 
+};
+
+// Import the necessary account and program types from our state module
 use crate::state::accounts::*;
 
+// Define a function to delete a user's subscription data
 pub fn delete(
-    _ctx: Context<Delete>
+    _ctx: Context<Delete> // The function takes a context object of type Delete
 ) -> Result<()> {
-    Ok(())
+    Ok(()) // The function simply returns a successful result
 }
+
+// Define a struct to represent the accounts needed for the delete operation
 #[derive(Accounts)]
 pub struct Delete<'info> {
+    // The enterprise_data account must be mutable and is derived from a seed
     #[account(mut, seeds = [b"Enterprise", enterprise_data.authority.key().as_ref()], bump = enterprise_data.bump_original)]
     pub enterprise_data: Account<'info, EnterpriseData>,
+
+    // The user_data account must be mutable, is derived from two seeds, and will be closed upon deletion
     #[account(mut, seeds = [enterprise_data.key().as_ref(), user.key().as_ref()], bump = user_data.bump, close = enterprise_data)]
     pub user_data: Account<'info, SubscriberData>,
+
+    // The user account must be a signer (i.e. it must be authorized to perform the delete operation)
     pub user: Signer<'info>,
+
+    // The system_program account is a standard Solana program account used for system-level operations
     pub system_program: Program<'info, System>,
 }
